085fa332fa75110a42a39ce7229a5e7f8a457a33
Joel Alenchery <joel@alenchery.org>
2014-09-07 04:12:45 +0530

    step-07
    http server with delay

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ var server = http.createServer(function(req, res) {
   res.writeHead(200, {
     'content-type': 'text/plain'
   });
-  res.write('Hello world\n');
-  res.end();  
+  res.write('Hello\n');
+
+  setTimeout(function() {
+    res.write('world!\n');
+    res.end();
+  }, 2000);
+  
 }).listen(3000);
 
 console.log('server running on port: 3000');
