8ff993c7d1aafb07683cbbf8287939a422ec5078
Joel Alenchery <joel@alenchery.org>
2014-09-07 00:54:02 +0530

    step-04
    - added sigint processing

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 setInterval(function(){
   console.log('world!');
-}, 1500);
+}, 1000);
 console.log('hello');
+
+process.on('SIGINT', function() {
+  console.log('Got SIGINT. Terminating.');
+  process.exit(0);
+});
