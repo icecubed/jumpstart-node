d3e5044af00a1a4d547a2eb296f9d42208cafdf2
Joel Alenchery <joel@alenchery.org>
2014-09-07 08:33:44 +0530

    step-08
    simple commonjs module

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 /*jslint node : true */
 var http = require('http');
+var config = require('./config/config.js');
 
 var server = http.createServer(function(req, res) {
   res.writeHead(200, {
     'content-type': 'text/plain'
   });
   res.write('Hello\n');
-
   setTimeout(function() {
     res.write('world!\n');
     res.end();
   }, 2000);
-  
-}).listen(3000);
+}).listen(config.port);
 
-console.log('server running on port: 3000');
+console.log('server running on port:' + config.port);
