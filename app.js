fd13ec267639a84a902ae931a0c1fe2e8880a5f2
Joel Alenchery <joel@alenchery.org>
2014-09-07 13:29:55 +0530

    step-ea-02

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var visits = require('./routes/visits'); // -- the route
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/visits', visits); // -- the request assignment
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
