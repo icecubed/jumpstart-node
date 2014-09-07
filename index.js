7fbe80ff7f1c3d52397300bbbcad457af07e6532
Joel Alenchery <joel@alenchery.org>
2014-09-07 08:50:25 +0530

    step-09
    move request handler as a seperate module

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 /*jslint node : true */
 var http = require('http');
 var config = require('./config/config.js');
+var handler = require('./lib/handler.js');
 
-var server = http.createServer(function(req, res) {
-  res.writeHead(200, {
-    'content-type': 'text/plain'
-  });
-  res.write('Hello\n');
-  setTimeout(function() {
-    res.write('world!\n');
-    res.end();
-  }, 2000);
-}).listen(config.port);
+var server = http.createServer(handler).listen(config.port);
 
 console.log('server running on port:' + config.port);
